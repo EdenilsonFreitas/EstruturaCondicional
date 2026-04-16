3e301c3c8103c10e1e953ae85ec2bc4cb8f9849b
Edenilson Freitas <freitaseddie126@gmail.com>
2026-04-16 17:58:55 +0000

    superando

---
+++ list02.py
@@ -0,0 +1,18 @@
+
+# numero = int(input("Digite um número inteiro: "))
+# if numero % 2 == 0:
+#     print(f"O número {numero} é par.")
+# else:
+#     print(f"O número {numero} é impar.")
+
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+adicao = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+
+print(f"A soma dos números é {adicao}")
+print(f"A Subtração dos números é {subtracao}")
+print(f"A multiplicação dos números é {multiplicacao}")
+print(f"A divisão dos números é {numero1 / numero2}")
